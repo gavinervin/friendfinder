089664343d2553e75b11508e389b7c1ade77cb36
Thomas Ervin <gavinervin47@gmail.com>
2018-01-29 04:24:33 -0500

    fix server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var PORT = process.env.PORT  || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: 'application/*+json'}));
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}))
-app.use(bodyParser.text({ type: 'text/html' }))
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}));
+app.use(bodyParser.text({ type: 'text/html' }));
 
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
@@ -15,3 +15,7 @@ require('./app/routing/htmlRoutes.js')(app);
 app.listen(PORT, function() {
 	console.log('App listening on PORT:' + PORT);
 }); 
+
+
+
+
