b0813a499676332f9d8247a43052dc689283a808
Thomas Ervin <gavinervin47@gmail.com>
2018-01-29 04:25:41 -0500

    add friends

---
+++ app/data/friends.js
@@ -0,0 +1,65 @@
+var friends = [
+
+{
+	name: "Ricky",
+	photo: "http://trailerpark.wikia.com/wiki/Ricky",
+	scores: [
+	"3",
+	"2",
+	"3",
+	"5",
+	"1"
+	]
+},
+
+{
+	name: "Randy",
+	photo: "https://vignette.wikia.nocookie.net/trailerpark/images/d/d4/Randy-trailer-park-boys.jpg/revision/latest?cb=20130723144956",
+	scores: [
+	"2",
+	"2",
+	"2",
+	"2",
+	"2"
+	]
+  },
+
+  {
+	name: "Julian",
+	photo: "https://vignette.wikia.nocookie.net/trailerpark/images/7/73/7376012_sma.jpg/revision/latest?cb=20130723144419",
+	scores: [
+	"3",
+	"2",
+	"4",
+	"5",
+	"5"
+	]
+  },
+
+  {
+	name: "Bubbles",
+	photo: "https://channel45news.com/wp-content/uploads/2017/05/6f946e6a00cf65f9a9d4eb71289ad252-696x928.jpg",
+	scores: [
+	"2",
+	"3",
+	"1",
+	"1",
+	"1"
+	]
+  },
+
+  {
+	name: "Jim Lahey",
+	photo: "https://i.warosu.org/data/ck/img/0037/97/1344584484234.jpg",
+	scores: [
+	"5",
+	"1",
+	"1",
+	"1",
+	"5"
+	]
+  }
+
+
+];
+module.exports = friends;
