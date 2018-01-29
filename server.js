a10d10eb3bfd5fd29aa17c769dedb6f815e0d031
Thomas Ervin <gavinervin47@gmail.com>
2018-01-28 21:02:00 -0500

    final server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var bodyParser = require('body-parser');
+var path = require('path');
 var express= require('express');
-
 var app = express();
 var PORT = process.env.PORT  || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/*+json'}));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}))
+app.use(bodyParser.text({ type: 'text/html' }))
 
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
